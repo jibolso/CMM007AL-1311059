1d6f81899776834c4cd7937be6dfeaae605eb1a7
1311059 <a.e.obayemi@rgu.ac.uk>
2016-05-04 11:06:00 +0100

    Assessed Lab, PHP + adminer

---
+++ php/add.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>myBlog | Add</title>
+    <link href="../assets/css/main.css" rel="stylesheet" />
+    <link href='https://fonts.googleapis.com/css?family=Pacifico' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Dosis' rel='stylesheet' type='text/css'>
+</head>
+<body>
+<header>
+    <div id="title">
+        <span id="mainTitle"><a href="../index.html">myBlog</a> </span><br>
+        <span id="subTitle">because the internet needs to know what i think</span>
+    </div>
+    <nav>
+        <ul>
+            <li><a href="../blog.html">All Blog Items</a> </li>
+            <li><a href="../blog.html">Work Items</a> </li>
+            <li><a href="../blog.html">University Items</a> </li>
+            <li><a href="../blog.html">Family Items</a> </li>
+            <li><a href="../add.html">Insert a Blog Item</a> </li>
+
+        </ul>
+    </nav>
+</header>
+<main>
+        <div id="add">
+            <form name="#" action="#" method="#">
+                <div>
+                    <label for="entryTitle">Entry Title</label>
+                    <input type="text" id="entryTitle" name="entryTitle" value="" required/>
+                </div>
+
+                <div>
+                    <label for="entrySummary">Entry Summary</label>
+                    <textarea name="entrySummary" id="entrySummary" rows="10" cols="10" required></textarea>
+                </div>
+
+                <div>
+                    <label for="entryCategory">Category</label>
+                    <select name="entryCategory" id="entryCategory" required>
+                        <option value="">Select</option>
+                        <option value="Work">Work</option>
+                        <option value="University">Sport</option>
+                        <option value="Family">Family</option>
+                    </select>
+                </div>
+
+                <div>
+                    <label for="submittedBy">Submitted By</label>
+                    <input type="text" id="submittedBy" name="submittedBy" value="" />
+                </div>
+                <div>
+                    <button type="submit" id="submit">Submit</button>
+                </div>
+            </form>
+        </div>
+
+</main>
+<footer>
+    <span><hr></span>
+    <p>Designed by Ajibola Obayemi, &copy; 2016</p>
+</footer>
+
+</body>
+</html>
---
+++ php/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>myBlog | Home</title>
+    <link href="../assets/css/main.css" rel="stylesheet" />
+    <link href='https://fonts.googleapis.com/css?family=Pacifico' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Dosis' rel='stylesheet' type='text/css'>
+</head>
+<body>
+<header>
+    <div id="title">
+        <span id="mainTitle"><a href="../index.html">myBlog</a> </span><br>
+        <span id="subTitle">because the internet needs to know what i think</span>
+    </div>
+    <nav>
+        <ul>
+            <li><a href="../blog.html">All Blog Items</a> </li>
+            <li><a href="../blog.html">Work Items</a> </li>
+            <li><a href="../blog.html">University Items</a> </li>
+            <li><a href="../blog.html">Family Items</a> </li>
+            <li><a href="../add.html">Insert a Blog Item</a> </li>
+
+        </ul>
+    </nav>
+</header>
+<main>
+    <section>
+        <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+            It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+            It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker
+            including versions of Lorem Ipsum.
+        </p>
+    </section>
+    <aside>
+        <img src="../assets/images/blog.png" alt="myBlog Image" width="200px"/>
+    </aside>
+</main>
+<footer>
+    <span><hr></span>
+    <p>Designed by Ajibola Obayemi, &copy; 2016</p>
+</footer>
+
+</body>
+</html>
